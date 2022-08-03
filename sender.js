26d265b47607d06bb0a714b70a04e2d1fea9aed9
The-Naty <youssef-el-zanaty@outlook.com>
2022-08-03 06:32:59 +0200

    Update sender.js
    initialized message sender.

--- sender.js
+++ sender.js
@@ -2,4 +2,7 @@ var amqp = require("amqplib/callback_api");
 
 amqp.connect("amqp://localhost", function (error0, connection) {
   if (error0) {
-    throw errosadas
+    throw error0;
+  }
+  connection.createChannel(function (error1, channel) {});
+});
