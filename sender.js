25d2c9d88681eba9cb138823f065345099888168
The-Naty <youssef-el-zanaty@outlook.com>
2022-08-03 16:07:54 +0200

    Docker and publisher
    connected to RabbitMQ image using docker containerization, created publisher.js instead of sender.js to send/publish messages.

--- sender.js
+++
@@ -1,8 +0,0 @@
-var amqp = require("amqplib/callback_api");
-
-amqp.connect("amqp://localhost", function (error0, connection) {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel(function (error1, channel) {});
-});
